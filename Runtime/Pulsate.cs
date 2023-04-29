bfea7c8fdac9144bae24b7e8a27ee4a0fd121a19
billbobphil <37590467+billbobphil@users.noreply.github.com>
2023-04-29 09:58:50 -0700

    Added option for pulsate to operate outside of timescale window if required to fit with simple pause methods.

--- Runtime/Pulsate.cs
+++ Runtime/Pulsate.cs
@@ -10,22 +10,33 @@ namespace Utilities
         public float scaleIncrement;
         private bool _isGrowing = true;
         private Vector3 _originalScale;
+        public bool useTimeScale;
 
         private void Start()
         {
             _originalScale = transform.localScale;
         }
         
+        private void Update() {
+            if(useTimeScale) return;
+
+            DetermineIsGrowing();
+
+            if (_isGrowing)
+            {
+                transform.localScale += new Vector3(scaleIncrement * Time.unscaledDeltaTime, scaleIncrement * Time.unscaledDeltaTime, 0);
+            }
+            else
+            {
+                transform.localScale -= new Vector3(scaleIncrement * Time.unscaledDeltaTime, scaleIncrement * Time.unscaledDeltaTime, 0);
+            }
+        }
+        
         private void FixedUpdate()
         {
-            if (transform.localScale.x >= maxScale.x && transform.localScale.y >= maxScale.y)
-            {
-                _isGrowing = false;
-            }
-            else if (transform.localScale.x <= minScale.x && transform.localScale.y <= minScale.y)
-            {
-                _isGrowing = true;
-            }
+            if(!useTimeScale) return;
+
+            DetermineIsGrowing();
 
             if (_isGrowing)
             {
@@ -34,6 +45,17 @@ namespace Utilities
             else
             {
                 transform.localScale -= new Vector3(scaleIncrement, scaleIncrement, 0);
+            }
+        }
+
+        private void DetermineIsGrowing() {
+            if (transform.localScale.x >= maxScale.x && transform.localScale.y >= maxScale.y)
+            {
+                _isGrowing = false;
+            }
+            else if (transform.localScale.x <= minScale.x && transform.localScale.y <= minScale.y)
+            {
+                _isGrowing = true;
             }
         }
 
